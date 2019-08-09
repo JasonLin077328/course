74ea7f5727f8bba3776e5ded16fda96b0d1d5464
JasonLin <jason077328@gmail.com>
2019-08-09 17:20:20 +0800

    jasontest

--- course/Controllers/HomeController.cs
+++ course/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace course.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();//test
         }
 
         public ActionResult About()
